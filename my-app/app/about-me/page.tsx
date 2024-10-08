3b4df199097512e138be574118d88bb7c5d1ab54
Preston <preston.ang.2022@smu.edu.sg>
2024-10-08 11:34:49 +0800

    go

--- my-app/app/about-me/page.tsx
+++ my-app/app/about-me/page.tsx
@@ -1,3 +1,27 @@
-export default function Home() {
-  return (  <div>Hello World! This is the about-me page</div>);
+import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card";
+
+export default function AboutMe() {
+  return (
+    <div>
+        Hello world this is the about me route
+        <Card>
+            <CardHeader>
+                <CardTitle>
+                    Hello I am Preston
+                </CardTitle>
+                <CardDescription>
+                    I like to code lots
+                </CardDescription>
+            </CardHeader>
+            <CardContent>
+                <div className="flex flex-row gap-2">
+                    <p className="font-bold">Name:</p>
+                    Preston
+                </div>
+                <div className="flex flex-row gap-2"><p className="font-bold">Major:</p>Information Systems(?)</div>
+                <div className="flex flex-row gap-2"><p className="font-bold">Hobbies:</p>Music</div>
+            </CardContent>
+        </Card>
+    </div>
+  );
 }
